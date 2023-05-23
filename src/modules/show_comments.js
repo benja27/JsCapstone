706d21c72ee96038d853a62e85e52d2cedd52337
benja27 <50673336+benja27@users.noreply.github.com>
2023-05-22 22:58:25 -0600

    add comment section

--- src/modules/show_comments.js
+++ src/modules/show_comments.js
@@ -8,28 +8,25 @@ const showComments = (index, arrayOfPokemons) => {
   modal.innerHTML = `
   
     
-  <div class="card col-md-8">
+  <div class="card col-10 col-md-8">
     
     <div class="card-body  d-sm-flex ">
+
       <div class="button-close-modal" >
         <i class="fa-solid fa-xmark" ></i>
       </div>
 
-      <div class="col-md-6" >
+      <div class="col-sm-6" >
         <img src=${arrayOfPokemons[index].picture} alt="">
         <h4 class="text-center" > ${arrayOfPokemons[index].name} </h4>
         <h6>Hablity #1:  ${arrayOfPokemons[index].mov1} </h6>
         <h6>hability #2:  ${arrayOfPokemons[index].mov2}</h6>
         <h5>Comments(2)</h5>
         <h6>fecha  comment</h6>
-        <h6>fecha  comment</h6>
-        <h6>fecha  comment</h6>
-        <h6>fecha  comment</h6>
-        <h6>fecha  comment</h6>
-        <h6>fecha  comment</h6>
+        <h6>fecha  comment</h6>        
       </div>
       
-      <div class="col-md-6 d-flex align-items-center" >
+      <div class="col-sm-6 d-flex align-items-center" >
         <form action="" class="d-flex flex-column justify-content-aroun" >
           <div class="mb-3">            
             <h6>Add a comment</h6>
